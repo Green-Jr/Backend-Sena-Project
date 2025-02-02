65463ddc68ac64481eaaebba561319996568ad40
Arbey Orozco Escobar <Junikoescobar11@gmail.com>
2025-02-02 14:10:40 -0500

    Ajuste para prod final

--- src/infrastructure/index.js
+++ src/infrastructure/index.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Cambia esto por el dominio del frontend
+  origin: process.env.FRONTEND, // Cambia esto por el dominio del frontend
   methods: 'GET,POST,PUT,DELETE',
   credentials: true,
 }));
